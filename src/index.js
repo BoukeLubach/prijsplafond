695a333916ef8cf663a15b6b010cf24f22026b05
Bouke Lubach <b.lubach@waterenergysolutions.com>
2022-09-23 20:24:44 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 // React Required
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 // Create Import File
 import './index.scss';
-
 import PageScrollTop from './component/PageScrollTop';
-
-
-
+import Home from './pages/Home';
 // Element Layout
 import error404 from "./elements/error404";
-
 // Blocks Layout
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import Home from './pages/Home';
 
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 
 class Root extends Component{
     render(){
@@ -25,7 +30,7 @@ class Root extends Component{
                 <PageScrollTop>
                     <Switch>
 
-                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home}/>
+                        <Route exact path={`${process.env.PUBLIC_URL}/home`} component={Home}/>
 
                         
                         <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
